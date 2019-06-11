4bde4e587d53e98d7c17b5572f720b280d0fa8bd
Alicia Bendz <alicia.bendz@nordstrom.com>
2019-06-10 18:45:12 -0700

    Insertion sort

--- file_manager/file_sorter.py
+++ file_manager/file_sorter.py
@@ -1,49 +1,34 @@
 # implement insertion sort and merge sort on a single file
 class FileSort:
-    def insertion_sort(file):
-        # assumes no extension
-        name = file.get_filename() + "_processed"
-        lines = []
-
-        with open(file.get_path() + file.get_filename(), 'r') as f:
-            lines = f.readlines()
-
+    def insertion_sort(lines):
+        split_lines = [None] * len(lines)
         for i in range(0, len(lines)):
-            lines[i] = lines[i].split(',')
+            split_lines[i] = lines[i].strip().split(',')
 
         # Now there is a list of list where the inner list is
         # [timestamp, latitude, longitude]
         # sort by timestamp
 
         # Note: this does duplicate space needed
-        sorted = None * len(lines)
-        end_index = 0
+        sorted = [None] * len(lines)
+        sorted[0] = split_lines[0]
+        split_lines = split_lines[1:]
 
-        for line in lines:
-            timestamp_to_sort = line[i][0]
-            index = end_index
-            timestamp_sorted = sorted[end_index]
+        sorted_length = 1
 
-            # For the first line sorted[0] will be none
-            if timestamp_sorted == None:
-                sorted[end_index] = timestamp_to_sort
-                continue
+        for data in split_lines:
+            timestamp = data[0]
+            last_sorted = sorted[sorted_length - 1][0]
+            current_sorted_index = sorted_length - 1
 
-            # get the right most timestampe
-            timestamp_sorted = sorted[index][0]
+            while last_sorted > timestamp and current_sorted_index >= 0:
+                sorted[current_sorted_index + 1] = sorted[current_sorted_index]
+                current_sorted_index = current_sorted_index - 1
+                if current_sorted_index >= 0:
+                    last_sorted = sorted[current_sorted_index][0]
 
-            # Go backwards through the list looking for the first value
-            # less than the value to insert
-            while(timestamp_to_sort < timestamp_sorted and index >= 0):
-                # shift the current value right
-                sorted[index + 1] = sorted[index]
-
-                # move to the next left element to compare
-                index = index - 1
-                timestamp_sorted = sorted[index][0]
-
-            sorted[index] = timestamp_to_sort
-            end_index = end_index + 1
+            sorted[current_sorted_index + 1] = data
+            sorted_length = sorted_length + 1
 
         return sorted
 
--- file_manager/test_file_sorter.py
+++ file_manager/test_file_sorter.py
@@ -1,15 +1,28 @@
 import unittest
+from file_sorter import FileSort
 
 class SortingTest(unittest.TestCase):
 
-    def test(self):
+    def test_insertion(self):
+        result = FileSort.insertion_sort(self.lines)
+        self.assertEqual(self.sorted, result)
+
+    def test_merge(self):
+        # print(FileSort.quick_sort(self.lines))
+        return True
+
+    def test_quick(self):
+        # print(FileSort.merge_sort(self.lines))
         return True
 
     def setUp(self):
-        return True
+        self.lines = []
+        with open('file.csv', 'r') as f:
+            self.lines = f.readlines()
 
-    def tearDown(self):
-        return True
+        name = self.lines[0]
+        self.lines = self.lines[1:]
+        self.sorted = [['2019-06-07 07:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-07 11:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-07 15:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-08 04:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-08 06:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-08 23:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-09 03:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-09 12:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-10 01:28:22.897527', '122.3320708', '7.6062095'], ['2019-06-10 04:28:22.897527', '122.3320708', '7.6062095']]
 
 if __name__ == '__main__':
     unittest.main()
