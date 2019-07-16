805a5342b70e825b01df56c77a630ad105d14cb6
albendz <alicia.bendz@gmail.com>
2019-07-15 18:53:01 -0700

    Demo runs, though still with missing data; change name of file_map

--- file_manager/demo.py
+++ file_manager/demo.py
@@ -1,7 +1,11 @@
 ''' This is pseudocode and doesn't work yet '''
 
 from tracking_file import TrackingFile
-from file_map import AnimalTracker
+from AnimalTracker import AnimalTracker
+from file_sorter import FileSort
+from file_search_tree import BinarySearchNode
+from datetime import datetime
+from data_graph import Graph
 
 # Get some files for our Sharks
 thomas_data = TrackingFile('thomas', 'file.csv', './')
@@ -41,13 +45,13 @@ thomas = animal_data.get_animal("thomas")
 
 # Note that this node is the last one added
 # the queue_node was the first one added
-stack_node = thomas.get_unprocessed.peek()
+stack_node = thomas.get_unprocessed().peek()
 print(stack_node)
 
 # Sorting: sort a file contents and add it to processed
-print(brunswick.get_unprocessed().get_length())
+print(brunswick.get_unprocessed().get_count())
 queue_node = brunswick.get_unprocessed().pop()
-print(brunswick.get_unprocessed().get_length())
+print(brunswick.get_unprocessed().get_count())
 
 with open(queue_node.data.get_path() + queue_node.data.get_filename(), 'r') as f:
     lines = f.readlines()
@@ -63,27 +67,27 @@ print(sorted)
 start_range = sorted[0][0]
 end_range = sorted[-1][0]
 
-sorted_file = queue_node.data.get_path() + queue_node.data.get_filename() + "_sorted")
+sorted_file_name = queue_node.data.get_path() + queue_node.data.get_filename() + "_sorted"
 
-with open(sorted_file, 'w') as f:
-    f.write(start_range + ',' + end_range)
+with open(sorted_file_name, 'w') as f:
+    f.write(start_range + ',' + end_range + '\n')
     for line in sorted:
-        f.write(line)
+        f.write(','.join(line) + '\n')
 
-sorted_file = TrackingFile("brunswick", , queue_node.data.get_filename() + "_sorted", queue_node.data.get_path())
+sorted_file = TrackingFile("brunswick", queue_node.data.get_filename() + "_sorted", queue_node.data.get_path())
 brunswick.get_processed().insert_last(sorted_file)
 
 # Add sorted files into binary search tree
-brunswick.get_search_tree().add_node(BinarySearchNode(sorted_file))
+brunswick.search_tree = (BinarySearchNode(sorted_file))
 brunswick.get_search_tree().add_node(BinarySearchNode(sorted_file))
 brunswick.get_search_tree().add_node(BinarySearchNode(sorted_file))
 
 # Search for the file for a specific time
-brunswick.get_search_tree().find_node_with_timestamp("")
+brunswick.get_search_tree().find_node_with_timestamp(datetime.fromisoformat('2019-06-08 23:28:22.897527'))
 
 # Create a graph from the animals tracking information
 graph = Graph()
-graph.add_data_from_file(sorted_file)
+graph.add_data_from_file(sorted_file_name)
 
 # Determinine the location the animal visits most often
 print(graph.most_visited())
